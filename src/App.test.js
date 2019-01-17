864f5c7f6f5f3e3b2c716607c5b48239781fd6aa
juan <juan@kodefox.com>
2019-01-17 18:32:31 +0700

    last modification for app-test

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,12 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 it("render an image from uri", () => {
-  const app = document.createElement("div");
-  ReactDOM.render(<App />, app);
+  const div = document.createElement("div");
+  const header = document.createElement("header");
+  const image = document.createElement("image");
+  header.appendChild(image);
+  div.appendChild(header);
+
+  ReactDOM.render(<App />, div);
   // ReactDOM.unmountComponentAtNode(div);
 });
