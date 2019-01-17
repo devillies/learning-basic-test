90c36d205761b752bf370e9c4de6536864b0ebc8
juan <juan@kodefox.com>
2019-01-17 18:23:35 +0700

    modify the app test

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 it("render an image from uri", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+  const app = document.createElement("div");
+  ReactDOM.render(<App />, app);
   // ReactDOM.unmountComponentAtNode(div);
 });
